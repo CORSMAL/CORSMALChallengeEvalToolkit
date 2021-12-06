ab9519222c9996f222b730fe09badfad1cbecefd
ax300 <a.xompero@qmul.ac.uk>
2021-12-06 16:01:39 +0000

    updated evaluation for winter school

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # The CORSMAL Challenge evaluation toolkit
 
-This repository contains the official evaluation toolkit for the CORSMAL Challenge.
-The version of this toolkit can be used only with the train set of the
+This repository contains the official evaluation toolkit for the CORSMAL Challenge at the [2021 Intelligent Sensing Winter School](http://cis.eecs.qmul.ac.uk/school2021.html). 
+The version of this toolkit can be used only with the training set of the
 [CORSMAL Containers Manipulation dataset](http://corsmal.eecs.qmul.ac.uk/containers_manip.html).
 The toolkit provides transparency in how the evaluation is performed, and 
 enables the verification of the output using example submissions with annotation
  or random values as estimations.
 
-The repository also contains the submission forms as example for the train set,
+The repository also contains the submission forms as example for the training set,
 annotation files (including filling densities per container) and a demo with 
 examples of submissions (see [metadata.md](submissions/metadata.md) for details 
 of each submission using the annotation values, random-generated values, and
@@ -19,40 +19,13 @@ The script can be used to integrate novel solutions. More details in the
 
 Documents:
 * Technical details of performance measures and scores: [PDF](docs/PerformanceScores.pdf)
-* Submission form for the public testing set: [here](docs/submission_forms/public_test_set.csv)
+* PowerPoint template for preparing the presentation of the solution on Friday 10 December: [PPT](docs/2020.12.10_Presentation_Template.pptx)
 
 Requirements:
 * Python (Tested with 3.8.3)
 * Numpy (Tested with 1.18.5)
 * Pandas (Tested with 1.2.3)
 * Scikit-learn (Tested with 0.24.1)
-
-
-## Challenge venues
-
-<!-- Challenge is still on-going and could happen in future venues. Stay tuned! -->
-
-### Current venue
-<!-- [CORSMAL Challenge](http://corsmal.eecs.qmul.ac.uk/challenge.html) -->
-Audio-visual object classification for human-robot collaboration @[2022 IEEE International Conference on Acoustic, Speech, and Signal Processing](http://corsmal.eecs.qmul.ac.uk/challenge.html)
-
-**Important dates**
-| Date             |                             |
-|------------------|-------------------------------------|
-| January 10, 2022 | Release of the public test data set |
-| January 24, 2022 (11.59 PDT) | Submission of (i) papers, (ii) estimation results (T1-T5) on the public test data set, and (iii) and source code |
-| February 10, 2022 (11.59 PDT) |  Paper acceptance notification and release of the results in the leaderboards |
-| January 17, 2022 (11.59 PDT) | Camera-ready papers due |
-
-
-Papers must be formatted according to the instructions in the 
-[ICASSP 2022 Paper Kit](https://2022.ieeeicassp.org/papers/paper_kit.php) and 
-submitted to corsmal-challenge@qmul.ac.uk. 
-
-
-### Past venues
-* Multi-modal fusion and learning for robotics @[2020 International Conference on Pattern Recognition](http://corsmal.eecs.qmul.ac.uk/ICPR2020challenge.html)
-* Multi-modal fusion and learning for robotics @[2020 Intelligent Sensing Summer School](http://cis.eecs.qmul.ac.uk/school2020.html)
 
 
 ## Data format
@@ -134,46 +107,16 @@ Execution time [ms]:
 * x: estimated value (>=0)
 
 
+For this challenge the 2021 Intelligent Sensing Winter School, only the column *Container capacity* should be filled with the estimation of the team solution. 
+
 ### Output
 
 The [evaluation script](evaluate.py) outputs a .csv file (e.g., 
-res_train_set.csv for the train set) with the results (percentage) of 
+res_training_set.csv for the training set) with the results (score in percentage) of 
 each submission per row: 
 
-| Team | s1 | s2 | s3 | s4 | s5 | s6 | s7 | s8 | s9 | s10 | Overall | JFTL | CMD |
-|------|----|----|----|----|----|----|----|----|----|-----|---------|------|-----|
-
-
-* s1:  score for the filling level classification task (T1)
-* s2:  score for the filling type classification task (T2)
-* s3:  score for the container capacity estimation task (T3)
-* s4:  score for the container width at the top estimation (container dimensions task, T5)
-* s5:  score for the container width at the bottom estimation (container dimensions task, T5)
-* s6:  score for the container height (container dimensions task, T5)
-* s7:  score for the container mass estimation task (T4)
-* s8:  score for the filling mass estimation (computed by the toolkit from T1, T2, and T3)
-* s9:  score for the object safety estimation (estimated by the simulator run by the organisers)
-* s10: score for the delivery accuracy (estimated by the simulator run by the organisers)
-* Overall: aggregation (average) of the previous 10 performance scores (in percentage) and weighed based on the number of tasks submitted.
-* JFTL: score for the joint filling type and level classification (additional leaderboard)
-* CMD: score for the group tasks of container capacity and dimensions estimation (additional leaderboard)
-
-Only submissions which include the source code for the evaluation on the 
-private CCM test set will valid for the ranking. Source codes that are not 
-reproducible will get a 0 score.
-
-Note: The score for filling mass estimation is not a linear combination of the 
-scores outputted for T1, T2, and T3. The score takes into consideration the 
-formula for computing the filling mass based on the estimations of each task
-for each configuration. s8 is weighed by the number of tasks executed among
-filling level classification, filling type classification, and container capacity
-estimation.
-
-Note that s9 and s10 are weighed by the number of tasks executed (up to 5), whereas
-s4, s5, and s6 are weighed by 3 as they belong to the same task (container dimensions
-estimation).
-
-More details in the technical document: [PDF](docs/PerformanceScores.pdf)
+| Team | score |
+|------|-------|
 
 
 ## Demo
@@ -197,40 +140,8 @@ source run_eval_demo.sh
 
 The [evaluation script](evaluate.py) can be run with the following options:
 * --submission: path and filename of the submission file (e.g., submissions/pub_test/teamname.csv)
-* --set: choose between train, test_pub, test_priv, and test_comb 
 
-Note that these example submissions do not contain estimations for object safety,
-distance, and angle difference, and hence results for the scores s8 and s8 are 0, and
-the overall score is up to maximum 80 instead of 100.
 
-For reproducibility, we report the results in the table below.
-
-| Team | s1 | s2 | s3 | s4 | s5 | s6 | s7 | s8 | s9 | s10 | overall | JFLT |
-|---|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
-| test1 | 100.00 | 100.00 | 100.00 | 100.00 | 100.00 | 100.00 | 100.00 | 100.00 | 0.00 | 0.00 | 80.00 | 100.00 |
-| test2 | 100.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 39.00 | 0.00 | 0.00 | 2.78 | 0.00 |
-| test3 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 100.00 | 0.00 | 0.00 | 0.00 | 2.00 | 0.00 |
-| test4 | 100.00 | 100.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 39.00 | 0.00 | 0.00 | 9.56 | 100.00 |
-| test5 | 100.00 | 100.00 | 100.00 | 0.00 | 0.00 | 0.00 | 0.00 | 100.00 | 0.00 | 0.00 | 24.00 | 100.00 |
-| test6 | 0.00 | 0.00 | 100.00 | 100.00 | 100.00 | 100.00 | 0.00 | 71.91 | 0.00 | 0.00 | 18.88 | 0.00 |
-| test7 | 0.00 | 0.00 | 100.00 | 0.00 | 0.00 | 0.00 | 0.00 | 71.91 | 0.00 | 0.00 | 3.44 | 0.00 |
-| test8 | 0.00 | 100.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 42.83 | 0.00 | 0.00 | 2.86 | 0.00 |
-| test9 | 0.00 | 0.00 | 0.00 | 100.00 | 100.00 | 100.00 | 0.00 | 0.00 | 0.00 | 0.00 | 6.00 | 0.00 |
-| test10 | 0.00 | 0.00 | 100.00 | 0.00 | 0.00 | 0.00 | 100.00 | 71.91 | 0.00 | 0.00 | 10.88 | 0.00 |
-| test11 | 100.00 | 100.00 | 100.00 | 0.00 | 0.00 | 0.00 | 100.00 | 100.00 | 0.00 | 0.00 | 40.00 | 100.00 |
-| test12 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 |
-| random1 | 32.01 | 26.68 | 21.72 | 24.62 | 20.87 | 36.24 | 16.73 | 42.83 | 0.00 | 0.00 | 22.17 | 9.60 |
-| random2 | 32.01 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 42.83 | 0.00 | 0.00 | 1.50 | 0.00 |
-| random3 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 16.73 | 0.00 | 0.00 | 0.00 | 0.33 | 0.00 |
-| random4 | 32.01 | 26.68 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 42.83 | 0.00 | 0.00 | 4.06 | 9.60 |
-| random5 | 32.01 | 26.68 | 21.72 | 0.00 | 0.00 | 0.00 | 0.00 | 42.83 | 0.00 | 0.00 | 7.39 | 9.60 |
-| random6 | 0.00 | 0.00 | 21.72 | 24.62 | 20.87 | 36.24 | 0.00 | 42.83 | 0.00 | 0.00 | 5.85 | 0.00 |
-| random7 | 0.00 | 0.00 | 21.72 | 0.00 | 0.00 | 0.00 | 0.00 | 42.83 | 0.00 | 0.00 | 1.29 | 0.00 |
-| random8 | 0.00 | 26.68 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 42.83 | 0.00 | 0.00 | 1.39 | 0.00 |
-| random9 | 0.00 | 0.00 | 0.00 | 24.62 | 20.87 | 36.24 | 0.00 | 0.00 | 0.00 | 0.00 | 1.63 | 0.00 |
-| random10 | 0.00 | 0.00 | 21.72 | 0.00 | 0.00 | 0.00 | 16.73 | 42.83 | 0.00 | 0.00 | 3.25 | 0.00 |
-| random11 | 32.01 | 26.68 | 21.72 | 0.00 | 0.00 | 0.00 | 16.73 | 42.83 | 0.00 | 0.00 | 11.20 | 9.60 |
-| average | 32.01 | 26.68 | 31.89 | 63.96 | 50.95 | 66.24 | 30.25 | 50.34 | 0.00 | 0.00 | 35.23 | 9.60 |
 
 ## Enquiries, Question and Comments
 
--- evaluate.py
+++ evaluate.py
@@ -8,7 +8,7 @@
 #         Email: corsmal-challenge@qmul.ac.uk
 #
 #  Created Date: 2020/08/25
-# Modified Date: 2021/11/07
+# Modified Date: 2021/12/06
 #
 # MIT License
 
@@ -44,76 +44,6 @@ import copy
 
 # from pdb import set_trace as bp
 
-def FillingTypeAndLevelMapping(f_type, f_level):
-	f_type_lvl = np.ones(f_type.shape[0]) * -1
-
-	if( max(f_level) == 2 ):
-		f_level[f_level==1] = 50
-		f_level[f_level==2] = 90
-	
-	idx0 = np.where( (f_type == 0) & (f_level == 0) )
-	idx1 = np.where( (f_type == 1) & (f_level == 50) )
-	idx2 = np.where( (f_type == 1) & (f_level == 90) )
-	idx3 = np.where( (f_type == 2) & (f_level == 50) )
-	idx4 = np.where( (f_type == 2) & (f_level == 90) )
-	idx5 = np.where( (f_type == 3) & (f_level == 50) )
-	idx6 = np.where( (f_type == 3) & (f_level == 90) )
-	
-	f_type_lvl[idx0] = 0
-	f_type_lvl[idx1] = 1
-	f_type_lvl[idx2] = 2
-	f_type_lvl[idx3] = 3
-	f_type_lvl[idx4] = 4
-	f_type_lvl[idx5] = 5
-	f_type_lvl[idx6] = 6
-	
-	return f_type_lvl
-
-
-def computeFillingMass(est, baseline, gt):
-	num_tasks = 3
-	num_tasks_completed = 3
-
-	# Replace -1 by the baseline result
-	if all(est['Filling type'] == -1):
-		est['Filling type'] = baseline['Filling type']
-		num_tasks_completed -= 1
-
-	if all(est['Filling level'] == -1):
-		est['Filling level'] = baseline['Filling level']
-		num_tasks_completed -= 1
-
-	if all(est['Container capacity'] == -1):
-		est['Container capacity'] = baseline['Container capacity']
-		num_tasks_completed -= 1
-
-	fl = est['Filling level'].values
-	fl[est['Filling level'].values==1] = 50
-	fl[est['Filling level'].values==2] = 90
-	
-	# mass = Filling level x capacity x densitiy(filling)
-	estimated_mass = fl/100. * est['Container capacity'].values * gt['filling density'].values
-
-	if not all(est['Filling level'] == -1):
-		estimated_mass[est['Filling level'] == -1] = -1
-
-	if not all(est['Container capacity'] == -1):
-		estimated_mass[est['Container capacity'] == -1] = -1
-
-	est['Filling mass'] = estimated_mass
-
-	return num_tasks_completed / num_tasks
-
-def computeWeightedAverageF1Score(gt, est):
-	assert (len(gt) == len(est))
-
-	if all(x == -1 for x in est):
-		return 0
-
-	gt = gt.astype(str)
-	est = est.astype(str)
-	return metrics.f1_score(gt, est, average='weighted')
-
 
 # Score computed for container capacity and mass estimation
 def computeScoreType1(gt, _est):
@@ -132,134 +62,9 @@ def computeScoreType1(gt, _est):
 	
 	return score
 
-def computeScoreType2(gt, _est):
-	est = copy.deepcopy(_est)
-
-	assert (len(gt) == len(est))
-
-	if all(x == -1 for x in est):
-		return 0
-
-	indicator_f = est > -1
-
-	ec = np.zeros(len(est))
-	err_abs = np.abs(est - gt);
-	
-	ec[err_abs < gt] = 1 - err_abs[err_abs < gt]/gt[err_abs < gt]
-	ec[err_abs >= gt] = 0
-
-	ec[(est == 0) * (gt == 0)] = 1
-
-	score = np.sum(ec * indicator_f) / len(gt)
-		
-	return score
-
-def computeScoreType3(gt, _est):
-	est = copy.deepcopy(_est)
-
-	assert (len(gt) == len(est))
-
-	if all(x == -1 for x in est):
-		return 0
-
-	indicator_f = est > -1
-
-	ec = est
-		
-	ec[(gt == 0) * (est == 0)] = 0
-	ec[(gt == 0) * (est != 0)] = est[(gt == 0) * (est != 0)]
-	ec[gt != 0] = np.abs(est[gt != 0] - gt[gt != 0]) / gt[gt != 0];
-
-	score = np.sum(np.exp(-ec) * indicator_f) / len(gt)
-		
-	return score
-
-
-def computeFillingLevelScore(gt, _est):
-	return computeWeightedAverageF1Score(gt, _est)
-
-def computeFillingTypeScore(gt, _est):
-	return computeWeightedAverageF1Score(gt, _est)
 
 def computeContainerCapacityScore(gt, _est):
 	return computeScoreType1(gt, _est)
-
-def computeContainerMassScore(gt, _est):
-	return computeScoreType1(gt, _est)
-
-def computeContainerWidthTopScore(gt, _est):
-	return computeScoreType2(gt, _est)
-
-def computeContainerWidthBottomScore(gt, _est):
-	return computeScoreType2(gt, _est)
-
-def computeContainerHeightScore(gt, _est):
-	return computeScoreType2(gt, _est)
-
-def computeFillingMassScore(gt, _est):
-	return computeScoreType3(gt, _est)
-
-def computeObjectSafetyScore(_est):
-	est = copy.deepcopy(_est)
-
-	if all(x == -1 for x in est):
-		return 0
-
-	indicator_f = est > -1
-
-	score = np.sum(est * indicator_f) / len(est)
-	return score
-
-def computeDeliveryAccuracyScore(_est_distance, _est_angle):
-	est_distance = copy.deepcopy(_est_distance)
-	est_angle = copy.deepcopy(_est_angle)
-
-	assert (len(est_distance) == len(est_angle))
-
-	indicator_f1 = est_distance > -1
-	indicator_f2 = est_angle > -1
-
-	assert (p==q for p,q in zip(indicator_f1,indicator_f2))
-
-	indicator_f = indicator_f1
-
-	eta = 500 # the maximum distance allowed from the pre-defined delivery location (in mm)
-	phi = math.pi / 4 # the value of β at which the container would tip over [in radians]
-
-	Delta = np.zeros(len(est_distance))
-
-	Delta[(est_distance < eta) * (est_angle < phi)] = 1 - est_distance[(est_distance < eta) * (est_angle < phi)]/eta
-
-	score = np.sum(Delta * indicator_f)
-
-	return score
-
-def computeJointFillingTypeLevelScore(gt, _est):
-	return computeWeightedAverageF1Score(gt, _est)
-
-
-def getTasksWeight(_est):
-	est = copy.deepcopy(_est)
-
-	num_tasks = 5
-	num_tasks_completed = 5
-
-	if all(x == -1 for x in est['Filling level'].values):
-		num_tasks_completed -= 1
-
-	if all(x == -1 for x in est['Filling type'].values):
-		num_tasks_completed -= 1
-
-	if all(x == -1 for x in est['Container capacity'].values):
-		num_tasks_completed -= 1
-
-	if all(x == -1 for x in est['Container mass'].values):
-		num_tasks_completed -= 1
-
-	if (all(x == -1 for x in est['Width at the top'].values)) and (all(x == -1 for x in est['Width at the bottom'].values)) and (all(x == -1 for x in est['Height'].values)):
-		num_tasks_completed -= 1
-
-	return num_tasks_completed / num_tasks
 
 
 if __name__ == '__main__':
@@ -267,126 +72,34 @@ if __name__ == '__main__':
 	# Arguments
 	parser = argparse.ArgumentParser(description='CORSMAL Challenge evaluation')
 	parser.add_argument('--submission', default='random.csv', type=str)
-	parser.add_argument('--set', default='train', help="Choose the set option:\n--train\n--test_pub\n--test_priv", choices=['train','test_pub','test_priv','test_comb'])
 	args = parser.parse_args()
 
-	if args.set == 'train':
-		outfile = 'res_train_set.csv'
-		offset = 0.047505
-		
-		annotationfile = 'annotations/ccm_train_annotation.csv'
-		baselinefile = 'submissions/train_set/random1.csv'
-		submissionfile = 'submissions/train_set/{}'.format(args.submission)
-
-		# Read annotations
-		gt = pd.read_csv(annotationfile, sep=',')
-
-		# Read baseline (random)
-		baseline = pd.read_csv(baselinefile, sep=',')
-
-		# Read submission
-		est = pd.read_csv(submissionfile, sep=',')
+	outfile = 'res_training_set.csv'
+	offset = 0.047505
 	
-	elif args.set == 'test_pub':
-		outfile = 'res_test_pub.csv'
-		offset = 0.004339
+	annotationfile = 'annotations/ccm_train_annotation.csv'
+	baselinefile = 'submissions/train_set/random1.csv'
+	submissionfile = 'submissions/train_set/{}'.format(args.submission)
+
+	# Read annotations
+	gt = pd.read_csv(annotationfile, sep=',')
+
+	# Read baseline (random)
+	baseline = pd.read_csv(baselinefile, sep=',')
+
+	# Read submission
+	est = pd.read_csv(submissionfile, sep=',')
 	
-		annotationfile = 'annotations/ccm_test_pub_annotation.csv'
-		baselinefile = 'submissions/pub_test_set/random1.csv'
-		submissionfile = 'submissions/pub_test_set/{}'.format(args.submission)
-
-		# Read annotations
-		gt = pd.read_csv(annotationfile, sep=',')
-
-		# Read baseline (random)
-		baseline = pd.read_csv(baselinefile, sep=',')
-
-		# Read submission
-		est = pd.read_csv(submissionfile, sep=',')
+	capacity_score  = computeContainerCapacityScore(gt['container capacity'].values, est['Container capacity'].values)
 	
-	elif args.set == 'test_priv':
-		outfile = 'res_test_priv.csv'
-		offset = 0.004338
 
-		annotationfile = 'annotations/ccm_test_priv_annotation.csv'
-		baselinefile = 'submissions/priv_test_set/random1.csv'
-		submissionfile = 'submissions/priv_test_set/{}'.format(args.submission)
-
-		# Read annotations
-		gt = pd.read_csv(annotationfile, sep=',')
-
-		# Read baseline (random)
-		baseline = pd.read_csv(baselinefile, sep=',')
-
-		# Read submission
-		est = pd.read_csv(submissionfile, sep=',')
-
-	elif args.set == 'test_comb':
-		outfile = 'res_test_comb.csv'
-		offset = 0.004338
-
-		annotationfiles = ['annotations/ccm_test_pub_annotation.csv', 'annotations/ccm_test_priv_annotation.csv']
-		baselinefiles = ['submissions/pub_test_set/random1.csv','submissions/priv_test_set/random1.csv']
-		submissionfiles = ['submissions/pub_test_set/{}'.format(args.submission),'submissions/priv_test_set/{}'.format(args.submission)]
-
-		# Read annotations
-		gt = pd.concat((pd.read_csv(f, sep=',') for f in annotationfiles), ignore_index=True)
-
-		# Read baseline (random)
-		baseline = pd.concat((pd.read_csv(f, sep=',') for f in baselinefiles), ignore_index=True)
-
-		# Read submission
-		est = pd.concat((pd.read_csv(f, sep=',') for f in submissionfiles), ignore_index=True)	
-
-	est_filling_mass = copy.deepcopy(est)
-	# Compute metrics
-	task_weight = getTasksWeight(est)
-	print(task_weight)
-
-	ntasks_filling_mass = computeFillingMass(est_filling_mass, baseline, gt)
-
-	est['Filling mass'] = est_filling_mass['Filling mass']
-
-	est_flt = FillingTypeAndLevelMapping(est['Filling type'].values, est['Filling level'].values)
-	gt_flt = FillingTypeAndLevelMapping(gt['filling type'].values, gt['filling level'].values)
-
-	est['Filling level'] = est['Filling level'].replace(50,1)
-	est['Filling level'] = est['Filling level'].replace(90,2)
-	gt['filling level'] = gt['filling level'].replace(50,1)
-	gt['filling level'] = gt['filling level'].replace(90,2)
-
-	s1  = computeFillingLevelScore(gt['filling level'].values, est['Filling level'].values)
-	s2  = computeFillingTypeScore(gt['filling type'].values, est['Filling type'].values)
-	s3  = computeContainerCapacityScore(gt['container capacity'].values, est['Container capacity'].values)
-	s4  = computeContainerWidthTopScore(gt['width at the top'].values, est['Width at the top'].values)
-	s5  = computeContainerWidthBottomScore(gt['width at the bottom'].values, est['Width at the bottom'].values)
-	s6  = computeContainerHeightScore(gt['height'].values, est['Height'].values)
-	s7  = computeContainerMassScore(gt['container mass'].values, est['Container mass'].values)
-	
-	if ntasks_filling_mass > 0:
-		s8  = computeFillingMassScore(gt['filling mass'].values, est['Filling mass'].values)
-		s8 += offset # offset to reach 1 from the annotations
-		s8 = s8 * ntasks_filling_mass
-	else:
-		s8 = 0
-	
-	s9  = computeObjectSafetyScore(est['Object safety'].values) # Evaluated in the simulator
-	s10 = computeDeliveryAccuracyScore(est['Distance'].values, est['Angle difference'].values) # Evaluated in the simulator
-
-	s11 = computeJointFillingTypeLevelScore(gt_flt, est_flt)
-	s12 = s3/2 + (s4 + s5 + s6)/6
-
-	challenge_score = (s1 + s2 + s3 + (s4 + s5 + s6)/3 + s7 + s8 + (s9 + s10) * task_weight) / 8 
-
-	scores = np.array([s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,challenge_score,s11,s12]) * 100
-
-	print(args.submission[:-4] + ';{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f}\n'.format(scores[0],scores[1],scores[2],scores[3],scores[4],scores[5],scores[6],scores[7],scores[8],scores[9],scores[10],scores[11],scores[12]))
+	print(args.submission[:-4] + ';{:.2f}\n'.format(capacity_score*100))
 	if not os.path.exists(outfile):
 	  results_file = open(outfile, 'w')
-	  results_file.write('Team;s1;s2;s3;s4;s5;s6;s7;s8;s9;s10;overall;JFLT;CMD\n')
+	  results_file.write('Team;score\n')
 	  results_file.close()
 
 	with open(outfile, 'a') as myfile:
-		myfile.write(args.submission[:-4] + ';{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f};{:.2f}\n'.format(scores[0],scores[1],scores[2],scores[3],scores[4],scores[5],scores[6],scores[7],scores[8],scores[9],scores[10],scores[11],scores[12]))
+		myfile.write(args.submission[:-4] + ';{:.2f}\n'.format(capacity_score*100))
 	
 	myfile.close()
