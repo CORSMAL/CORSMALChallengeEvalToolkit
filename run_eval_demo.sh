ff76e2f1a2deadd353da6a62635e2bab7bfa95a6
ax300 <a.xompero@qmul.ac.uk>
2021-12-06 16:09:00 +0000

    updated evaluation for winter school

--- run_eval_demo.sh
+++ run_eval_demo.sh
@@ -1,33 +1,28 @@
 #! /bin/bash
 
-DATA_SET=train
-# DATA_SET=test_pub
-# DATA_SET=test_priv
-# DATA_SET=test_comb
+python evaluate.py  --submission test1.csv 
+python evaluate.py  --submission test2.csv 
+python evaluate.py  --submission test3.csv 
+python evaluate.py  --submission test4.csv 
+python evaluate.py  --submission test5.csv 
+python evaluate.py  --submission test6.csv 
+python evaluate.py  --submission test7.csv 
+python evaluate.py  --submission test8.csv 
+python evaluate.py  --submission test9.csv 
+python evaluate.py  --submission test10.csv 
+python evaluate.py  --submission test11.csv 
+python evaluate.py  --submission test12.csv
 
-python evaluate.py --set $DATA_SET --submission test1.csv 
-python evaluate.py --set $DATA_SET --submission test2.csv 
-python evaluate.py --set $DATA_SET --submission test3.csv 
-python evaluate.py --set $DATA_SET --submission test4.csv 
-python evaluate.py --set $DATA_SET --submission test5.csv 
-python evaluate.py --set $DATA_SET --submission test6.csv 
-python evaluate.py --set $DATA_SET --submission test7.csv 
-python evaluate.py --set $DATA_SET --submission test8.csv 
-python evaluate.py --set $DATA_SET --submission test9.csv 
-python evaluate.py --set $DATA_SET --submission test10.csv 
-python evaluate.py --set $DATA_SET --submission test11.csv 
-python evaluate.py --set $DATA_SET --submission test12.csv
+python evaluate.py  --submission random1.csv 
+python evaluate.py  --submission random2.csv 
+python evaluate.py  --submission random3.csv 
+python evaluate.py  --submission random4.csv 
+python evaluate.py  --submission random5.csv 
+python evaluate.py  --submission random6.csv 
+python evaluate.py  --submission random7.csv 
+python evaluate.py  --submission random8.csv 
+python evaluate.py  --submission random9.csv 
+python evaluate.py  --submission random10.csv 
+python evaluate.py  --submission random11.csv 
 
-python evaluate.py --set $DATA_SET --submission random1.csv 
-python evaluate.py --set $DATA_SET --submission random2.csv 
-python evaluate.py --set $DATA_SET --submission random3.csv 
-python evaluate.py --set $DATA_SET --submission random4.csv 
-python evaluate.py --set $DATA_SET --submission random5.csv 
-python evaluate.py --set $DATA_SET --submission random6.csv 
-python evaluate.py --set $DATA_SET --submission random7.csv 
-python evaluate.py --set $DATA_SET --submission random8.csv 
-python evaluate.py --set $DATA_SET --submission random9.csv 
-python evaluate.py --set $DATA_SET --submission random10.csv 
-python evaluate.py --set $DATA_SET --submission random11.csv 
-
-python evaluate.py --set $DATA_SET --submission average.csv 
+python evaluate.py  --submission average.csv 
